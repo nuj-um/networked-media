6911d685bf344a876464e461e30ea5d21603afca
nuj-um <sji3678@nyu.edu>
2026-09-23 11:09:51 -0400

    changes

---
+++ webserver/public/ee-p1/main.js
@@ -0,0 +1,40 @@
+// // this is a shorthand for the event listener
+// window.onload = () => {
+//   console.log("page is fully loaded");
+// };
+
+// const windowFeatures = "left=100,top=100,width=320,height=320";
+// const handle = window.open(
+//   "https://www.youtube.com/watch?v=n-4U2hfMpnk",
+//   "qongqothwane",
+//   windowFeatures,
+// );
+// if (!handle) {
+//   // The window wasn't allowed to open
+//   // This is likely caused by built-in popup blockers.
+//   // …
+// }
+
+// const window2Features = "left=350,top=500,width=320,height=320";
+// const handle2 = window.open(
+//   "https://www.youtube.com/watch?v=B9vgipLTYJQ",
+//   "patapata",
+//   window2Features,
+// );
+// if (!handle2) {
+//   // The window wasn't allowed to open
+//   // This is likely caused by built-in popup blockers.
+//   // …
+// }
+
+// const window3Features = "left=350,top=500,width=320,height=320";
+// const handle3 = window.open(
+//   "https://www.youtube.com/watch?v=Q1UID0vEeqI",
+//   "malaika",
+//   window3Features,
+// );
+// if (!handle3) {
+//   // The window wasn't allowed to open
+//   // This is likely caused by built-in popup blockers.
+//   // …
+// }
